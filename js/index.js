c81980fde528a24a72481deb4abb197fb4736b25
ProgrammerRuslan <vos.nau.04@gmail.com>
2021-01-24 18:02:13 +0200

    add space to function

--- js/index.js
+++ js/index.js
@@ -13,18 +13,23 @@ function addBlock(){
     let todolistBtns = document.createElement('div')
     let todolistConfirm = document.createElement('button')
     let todolistDelete = document.createElement('button')
+
     todolistBlock.classList.add('todolist-block')
     todolistText.classList.add('todolist-text')
     todolistBtns.classList.add('todolist-btns')
     todolistConfirm.classList.add('todolist-confirm')
     todolistDelete.classList.add('todolist-delete')
+
     todolistText.innerText = document.querySelector('#todolist-input').value
+
     todolistBtns.append(todolistConfirm, todolistDelete)
     todolistBlock.append(todolistText, todolistBtns)
     document.querySelector('#todolist').append(todolistBlock)
+
     todolistConfirm.addEventListener('click', () => {
       todolistText.classList.add('active')
     })
+
     todolistDelete.addEventListener('click', () => {
       todolistBlock.remove()
     })
